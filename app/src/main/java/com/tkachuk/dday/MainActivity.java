ba6442eb4824125438943c182755a670a5b15cfa
draxvel <ntv3331998@gmail.com>
2018-07-26 20:02:31 +0300

    Updated MainActivity (added Intent).

--- app/src/main/java/com/tkachuk/dday/MainActivity.java
+++ app/src/main/java/com/tkachuk/dday/MainActivity.java
@@ -1,15 +1,35 @@
 package com.tkachuk.dday;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn_rate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+        initListener();
+    }
+
+    private void initView() {
+        btn_rate = findViewById(R.id.btn_rate);
+    }
+
+    private void initListener() {
+        btn_rate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, RatingActivity.class));
+            }
+        });
     }
 
     @Override
